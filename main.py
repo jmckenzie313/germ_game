4cdc322ec2cfe50186b39bb964e4029d35443e49
jmckenzie313 <118020593+jmckenzie313@users.noreply.github.com>
2023-12-28 15:26:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) 
+pygame.display.set_caption("Germ")
+
+BG = pygame.transform.scale(pygame.image.load("bg.png"), (WIDTH, HEIGHT))
+
+PLAYER_WIDTH = 25
+PLAYER_HEIGHT = 25
+
+SMALL_GERM_WIDTH = 25
+SMALL_GERM_HEIGHT = 25
+
+MED_GERM_WIDTH = 40
+MED_GERM_HEIGHT = 40
+
+LARGE_GERM_WIDTH = 60
+LARGE_GERM_HEIGHT = 60
+
+PLAYER_VEL = 5
+
+def draw(player, enemy_1, enemy_2, enemy_3):
+    WIN.blit(BG, (0, 0))
+
+    pygame.draw.circle(WIN, (34,139,34), [player.x, player.y], PLAYER_WIDTH, PLAYER_HEIGHT)
+    pygame.draw.circle(WIN, (124,252,0), [enemy_1.x, enemy_2.y], SMALL_GERM_WIDTH, SMALL_GERM_HEIGHT)
+    pygame.draw.circle(WIN, (255,165,0), [enemy_2.x, enemy_2.y], MED_GERM_WIDTH, MED_GERM_HEIGHT)
+    pygame.draw.circle(WIN, (148,0,211), [enemy_3.x, enemy_3.y], LARGE_GERM_WIDTH, LARGE_GERM_HEIGHT)
+
+    pygame.display.update()
+
+def main():
+    run = True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    enemy_1 = pygame.Rect(750, 900, SMALL_GERM_WIDTH, SMALL_GERM_HEIGHT)
+    enemy_2 = pygame.Rect(500, 250, MED_GERM_WIDTH, MED_GERM_HEIGHT)
+    enemy_3 = pygame.Rect(900, 50, LARGE_GERM_WIDTH, LARGE_GERM_HEIGHT)
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >=0:
+            player.x -= PLAYER_VEL
+        elif keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+        elif keys[pygame.K_UP] and player.y - PLAYER_VEL >= 0:
+            player.y -= PLAYER_VEL
+        elif keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
+            player.y += PLAYER_VEL
+
+        draw(player, enemy_1, enemy_2, enemy_3)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
