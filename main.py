c3a34d0f43c15fc4849dbf802dfa69be9ca59197
Jasmine Brotemarkle <jasmine.mckenzie313@gmail.com>
2024-01-03 22:44:21 -0500

    board adjustment

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import copy
+import math
 import pygame
-import time
 import random
 
-WIDTH, HEIGHT = 1000, 800
+from board import boards
+
+WIDTH, HEIGHT = 900, 950
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Germ")
 
 BG = pygame.transform.scale(pygame.image.load("bg.png"), (WIDTH, HEIGHT))
+OBSTACLES = copy.deepcopy(boards)
+PI = math.pi 
 
 PLAYER_WIDTH = 25
 PLAYER_HEIGHT = 25
@@ -20,18 +25,56 @@ MED_GERM_HEIGHT = 40
 LARGE_GERM_WIDTH = 60
 LARGE_GERM_HEIGHT = 60
 
-PLAYER_VEL = 5
+VEL = 2
 
+small_germ_dead = False
+med_germ_dead = False
+large_germ_dead = False
+
+player_image = pygame.transform.scale(pygame.image.load(f'assets/player_images/1.png'), (60, 60))
+small_germ_image = pygame.transform.scale(pygame.image.load(f'assets/germ_images/blue.png'), (60, 60))
+med_germ_img = pygame.transform.scale(pygame.image.load(f'assets/germ_images/orange.png'), (70, 70))
+large_germ_img = pygame.transform.scale(pygame.image.load(f'assets/germ_images/red.png'), (80, 80))
 
 def draw(player, enemy_1, enemy_2, enemy_3):
-    WIN.blit(BG, (0, 0))
-
-    pygame.draw.circle(WIN, (34, 139, 34), [player.x, player.y], PLAYER_WIDTH, PLAYER_HEIGHT)
-    pygame.draw.circle(WIN, (124, 252, 0), [enemy_1.x, enemy_2.y], SMALL_GERM_WIDTH, SMALL_GERM_HEIGHT)
-    pygame.draw.circle(WIN, (255, 165, 0), [enemy_2.x, enemy_2.y], MED_GERM_WIDTH, MED_GERM_HEIGHT)
-    pygame.draw.circle(WIN, (148, 0, 211), [enemy_3.x, enemy_3.y], LARGE_GERM_WIDTH, LARGE_GERM_HEIGHT)
+    WIN.blit(player_image, (player.x, player.y))
+    WIN.blit(small_germ_image, (enemy_1.x, enemy_1.y))
+    WIN.blit(med_germ_img, (enemy_2.x, enemy_2.y))
+    WIN.blit(large_germ_img, (enemy_3.x, enemy_3.y))
 
     pygame.display.update()
+
+def draw_obstacles():
+        num1 = ((HEIGHT - 50) // 32)
+        num2 = (WIDTH // 30)
+        for i in range(len(OBSTACLES)):
+            for j in range(len(OBSTACLES[i])):
+                if OBSTACLES[i][j] == 1:
+                    pygame.draw.circle(WIN, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+                if OBSTACLES[i][j] == 2:
+                    pygame.draw.circle(WIN, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+                if OBSTACLES[i][j] == 3:
+                    pygame.draw.line(WIN, "black", (j * num2 + (0.5 * num2), i * num1),
+                                    (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+                if OBSTACLES[i][j] == 4:
+                    pygame.draw.line(WIN, "black", (j * num2, i * num1 + (0.5 * num1)),
+                                    (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+                if OBSTACLES[i][j] == 5:
+                    pygame.draw.arc(WIN, "black", [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
+                                    0, PI / 2, 3)
+                if OBSTACLES[i][j] == 6:
+                    pygame.draw.arc(WIN, "black",
+                                    [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+                if OBSTACLES[i][j] == 7:
+                    pygame.draw.arc(WIN, "black", [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                    3 * PI / 2, 3)
+                if OBSTACLES[i][j] == 8:
+                    pygame.draw.arc(WIN, "black",
+                                    [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                    2 * PI, 3)
+                if OBSTACLES[i][j] == 9:
+                    pygame.draw.line(WIN, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                    (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 
 def enemyMove(enemy_1, enemy_2, enemy_3):
@@ -49,9 +92,9 @@ def enemyMove(enemy_1, enemy_2, enemy_3):
         elif direction == "right":
             enemy.x += 1
 
-            # Check if the enemy is off the screen
+            # Check if the enemy is off the WIN
         if enemy.x < 0 or enemy.x > WIDTH or enemy.y < 0 or enemy.y > HEIGHT:
-            # If the enemy is off the screen, reset its position
+            # If the enemy is off the WIN, reset its position
             enemy.x = random.randint(0, WIDTH)
             enemy.y = random.randint(0, HEIGHT)
 
@@ -66,20 +109,22 @@ def main():
     enemies = [enemy_1, enemy_2, enemy_3]
 
     while run:
+        WIN.fill('white')
+        draw_obstacles()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
-            player.x -= PLAYER_VEL
-        elif keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
-            player.x += PLAYER_VEL
-        elif keys[pygame.K_UP] and player.y - PLAYER_VEL >= 0:
-            player.y -= PLAYER_VEL
-        elif keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
-            player.y += PLAYER_VEL
+        if keys[pygame.K_LEFT] and player.x - VEL >= 0:
+            player.x -= VEL
+        elif keys[pygame.K_RIGHT] and player.x + VEL + player.width <= WIDTH:
+            player.x += VEL
+        elif keys[pygame.K_UP] and player.y - VEL >= 0:
+            player.y -= VEL
+        elif keys[pygame.K_DOWN] and player.y + VEL + player.height <= HEIGHT:
+            player.y += VEL
 
 
         enemyMove(enemy_1, enemy_2, enemy_3)
